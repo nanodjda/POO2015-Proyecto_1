4ec111d80c71da4fc92d70b3eeb98d559d4154c0
David Diaz <david.diaz95@outlook.com>
2015-09-24 11:38:10 -0600

    Ultima

---
+++ src/modelo/Categoria.java
@@ -0,0 +1,27 @@
+/**
+* David Diaz Aguilar - 2014004725
+* Arturo Luna Izaguirre - 2014110993
+* Esteban Chinchilla Fallas - 2014
+*/
+
+package modelo;
+
+/**
+ *
+ * @author David 24/09/2015
+ */
+public class Categoria {
+
+    /************** Variables **************/
+    private String nombre;
+
+    /************* Constructor *************/
+    public Categoria(String pNombre){
+        this.nombre = nombre;
+    }
+
+    /****************Metodos****************/
+
+    /*********** Getters/Setters ***********/
+
+}
---
+++ src/modelo/Tipo.java
@@ -0,0 +1,35 @@
+/**
+* David Diaz Aguilar - 2014004725
+* Arturo Luna Izaguirre - 2014110993
+* Esteban Chinchilla Fallas - 2014
+*/
+
+package modelo;
+
+/**
+ *
+ * @author David 24/09/2015
+ */
+public class Tipo {
+
+    /************** Variables **************/
+    private String nombre;
+
+    /************* Constructor *************/
+    public Tipo(String pNombre){
+        this.nombre = pNombre;
+    }
+
+    /****************Metodos****************/
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /*********** Getters/Setters ***********/
+
+}
